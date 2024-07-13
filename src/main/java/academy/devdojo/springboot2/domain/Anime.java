ce75b8e2fcbceb2161a7621b35f911727f197698
vinicius zanchetta <viniciuszanchettadev@gmail.com>
2024-07-13 10:24:48 -0300

    Adicionado método findById, que retorna um único Anime pelo ID, e modificado o arquivo application.yml

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -8,5 +8,4 @@ import lombok.Data;
 public class Anime {
     private String name;
     private Long id;
-
 }
--- src/main/java/academy/devdojo/springboot2/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot2/service/AnimeService.java
@@ -1,13 +1,24 @@
 package academy.devdojo.springboot2.service;
 
 import academy.devdojo.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime("DBZ",1L), new Anime("Berserk",2L));
+
     public List<Anime> listAll() {
-        return List.of(new Anime("DBZ",1L), new Anime("Berserk",2L));
+        return animes;
+    }
+    public Anime findById(long id) {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
